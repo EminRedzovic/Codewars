203e043393f44e4db60a440454fb3b05ea9bfda6
EminRedzovic <redzovicemin200@gmail.com>
2023-02-22 00:53:29 +0100

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// PRVI
-
+//=======================================================
+//1.
 // https://www.codewars.com/kata/544a54fd18b8e06d240005c0
 // const min = (arr, toReturn) => {
 //   let result;
@@ -18,9 +18,8 @@
 // };
 // min([3, 1, 2, 4, 5], "index");
 // min([3, 1, 2, 4, 5], "value");
-
-// DRUGI
-
+//=======================================================
+//2.
 // https://www.codewars.com/kata/576bb71bbbcf0951d5000044
 // const Count = (arr) => {
 //   let count = 0;
@@ -36,9 +35,8 @@
 //   return [count, count2];
 // };
 // Count([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]);
-
-// Treci
-
+//=======================================================
+//3.
 // https://www.codewars.com/kata/57cc981a58da9e302a000214
 // const limit = (arr,limit) =>{
 //   let nesto = true;
@@ -49,9 +47,8 @@
 //   });
 //   return nesto;
 // }
-
-// Cetvrti
-
+//=======================================================
+//4.
 // https://www.codewars.com/kata/511f11d355fe575d2c000001
 // const twoOldestAges = (arr) => {
 //   arr.sort((a, b) => b - a);
@@ -59,11 +56,9 @@
 //   return [arr[1], arr[0]];
 // };
 // twoOldest([1, 2, 10, 8]);
-
-// Peti
-
+//=======================================================
+//5.
 // https://www.codewars.com/kata/580a4734d6df748060000045
-
 // const isSortedAndHow = (arr) => {
 //   let counter = 0;
 //   let counter1 = 0;
@@ -89,8 +84,8 @@
 //     return "no";
 //   }
 // };
-
-// Sesti
+//=======================================================
+//6.
 // https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/javascript
 
 // const wave = (str) => {
@@ -110,22 +105,22 @@
 //   return words;
 // };
 // wave("asddas");
-
-// Sedmi
+//=======================================================
+//7.
 // https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
 
 // const isDivisible = (n, x, y) => {
 //   return n % x === 0 && n % y === 0;
 // };
-
-// Osmi
+//=======================================================
+//8.
 // https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c
 
 // const sortByLength = (array) => {
 //   return array.sort((a,b)=>a.length - b.length)
 // };
-
-// Deveti
+//=======================================================
+//9.
 // https://www.codewars.com/kata/5d5ee4c35162d9001af7d699
 // const sumOfMinimums = (array) => {
 //   let sum = 0;
@@ -143,32 +138,31 @@
 //   [5, 6, 7, 8, 9],
 //   [20, 21, 34, 56, 100],
 // ]);
-
-// Deseti
+//=======================================================
+//10.
 // https://www.codewars.com/kata/63cbe409959401003e09978b
-const setReducer = (array) => {
-  let b = [];
-  counter = 1;
-  for (let i = 0; i <= array.length - 1; i++) {
-    if (array[i] === array[i + 1]) {
-      counter++;
-    } else {
-      b.push(counter);
-      counter = 1;
-    }
-  }
-  array = b;
-  if (array.length > 1) {
-    return setReducer(array);
-  } else {
-    console.log(array[0]);
-    return array[0];
-  }
-};
-// // [1, 1, 1, 3, 2, 1]
-setReducer([9, 4, 1, 1, 1, 2, 3, 9, 4, 6, 2]);
-
-// Jedanaest
+// const setReducer = (array) => {
+//   let b = [];
+//   counter = 1;
+//   for (let i = 0; i <= array.length - 1; i++) {
+//     if (array[i] === array[i + 1]) {
+//       counter++;
+//     } else {
+//       b.push(counter);
+//       counter = 1;
+//     }
+//   }
+//   array = b;
+//   if (array.length > 1) {
+//     return setReducer(array);
+//   } else {
+//     console.log(array[0]);
+//     return array[0];
+//   }
+// };
+// setReducer([9, 4, 1, 1, 1, 2, 3, 9, 4, 6, 2]);
+//=======================================================
+//11.
 //https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
 // const repeatStr = (n, s) => {
 //   let word = "";
@@ -179,14 +173,16 @@ setReducer([9, 4, 1, 1, 1, 2, 3, 9, 4, 6, 2]);
 //   return word;
 // };
 // repeatStr(6, "I");
-// Dvanaest
+//=======================================================
+//12.
 //https://www.codewars.com/kata/53d32bea2f2a21f666000256/train/javascript
 // const largest=(n, array) =>{
 //   // Find the n highest elements in a list
 //   return [];
 // }
 // largest(2, [7,6,5,4,3,2,1])
-// Trinaest
+//=======================================================
+//13.
 //https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
 // const noSpace = (x) => {
 //   x = x.split("");
@@ -196,7 +192,8 @@ setReducer([9, 4, 1, 1, 1, 2, 3, 9, 4, 6, 2]);
 //   return x.join("");
 // };
 // noSpace("8 j 8   mBliB8g  imjB8B8  jl  B");
-// Cetrnaest
+//=======================================================
+//14.
 //https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
 // const countSheeps = (arrayOfSheep) => {
 //   let count = 0;
@@ -207,7 +204,8 @@ setReducer([9, 4, 1, 1, 1, 2, 3, 9, 4, 6, 2]);
 //   });
 //   return count;
 // };
-// Petnaest
+//=======================================================
+//15.
 //https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/javascript
 // const sumArray = (array) => {
 //   if (!array || array.length <= 2) return 0;
@@ -222,7 +220,8 @@ setReducer([9, 4, 1, 1, 1, 2, 3, 9, 4, 6, 2]);
 //   return sum;
 // };
 // sumArray([6, 2, 1, 8, 10]);
-// Sesnaest
+//=======================================================
+//16.
 //https://www.codewars.com/kata/5875b200d520904a04000003
 // const enough = (cap, on, wait) => {
 //   if (on + wait <= cap) {
